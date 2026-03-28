ed23750563cfc7be9c1d2464e964936a79fd67f1
Eneas <eneas0312@gmail.com>
2026-03-28 10:41:18 +0100

    #17, created User entity, edited tests to work with this User entity. Edited rest (Mapper, DTO's) to work with new User, also changed createUser in UserService so it works with new User

--- src/main/java/ch/uzh/ifi/hase/soprafs26/service/UserService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/service/UserService.java
@@ -12,6 +12,7 @@ import ch.uzh.ifi.hase.soprafs26.constant.UserStatus;
 import ch.uzh.ifi.hase.soprafs26.entity.User;
 import ch.uzh.ifi.hase.soprafs26.repository.UserRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -41,6 +42,8 @@ public class UserService {
 	public User createUser(User newUser) {
 		newUser.setToken(UUID.randomUUID().toString());
 		newUser.setStatus(UserStatus.OFFLINE);
+		newUser.setCreationDate(LocalDateTime.now());
+		newUser.setPoints(0);
 		checkIfUserExists(newUser);
 		// saves the given entity but data is only persisted in the database once
 		// flush() is called
@@ -63,16 +66,11 @@ public class UserService {
 	 */
 	private void checkIfUserExists(User userToBeCreated) {
 		User userByUsername = userRepository.findByUsername(userToBeCreated.getUsername());
-		User userByName = userRepository.findByName(userToBeCreated.getName());
 
 		String baseErrorMessage = "The %s provided %s not unique. Therefore, the user could not be created!";
-		if (userByUsername != null && userByName != null) {
+		if (userByUsername != null) {
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
-					String.format(baseErrorMessage, "username and the name", "are"));
-		} else if (userByUsername != null) {
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, String.format(baseErrorMessage, "username", "is"));
-		} else if (userByName != null) {
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, String.format(baseErrorMessage, "name", "is"));
+					String.format(baseErrorMessage, "username", "is"));
 		}
 	}
 }
