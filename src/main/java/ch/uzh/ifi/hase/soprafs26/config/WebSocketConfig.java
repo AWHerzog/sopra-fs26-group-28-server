031175a3e35645e76cffdb345889e33909596f13
Eneas <eneas0312@gmail.com>
2026-05-05 10:59:52 +0200

    "Revert "Merge pull request #137 from AWHerzog/bugs"
    This reverts commit 2d077f26cd588524b40834c4009f43f40d855672, reversing
    changes made to f99d6d690cc3846164f31c4ce02473c5f07ebd9e.

--- src/main/java/ch/uzh/ifi/hase/soprafs26/config/WebSocketConfig.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/config/WebSocketConfig.java
@@ -5,30 +5,15 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import org.springframework.scheduling.TaskScheduler;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
-import org.springframework.context.annotation.Bean;
 
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    @Bean
-    public TaskScheduler taskScheduler() {
-        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
-        scheduler.setPoolSize(1);
-        scheduler.setThreadNamePrefix("wss-heartbeat-");
-        scheduler.initialize();
-        return scheduler;
-    }
-    
-    
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic")
-            .setHeartbeatValue(new long[]{4000, 4000})  // send/expect heartbeat every 4s
-            .setTaskScheduler(taskScheduler());
+        config.enableSimpleBroker("/topic"); 
         config.setApplicationDestinationPrefixes("/app"); 
     }
 
